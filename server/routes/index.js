a4228a5064fda71990188bc057358420cccc7106
alishanti98 <alialshanti90@gmail.com>
2022-09-10 08:32:56 +0300

    handling logout

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,8 +7,10 @@ router.use(notFoundError);
 router.use(serverError);
 
 const { handleSignup, handleLogin } = require('../controllers/index');
+const { handleSignup , handleLogin , handleLogout} = require('../controllers/index');
 
 router.post('/signup', handleSignup);
 router.post('/login', handleLogin);
+router.post('/logout', handleLogout);
 
 module.exports = router;
